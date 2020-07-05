c1f6b810bf3953baf6ceac29557ae59d2cd24fbb
Reza-CyBer44 <64094365+Reza-CyBer44@users.noreply.github.com>
2020-07-05 13:32:48 +0430

    Update config.lua

--- DiscordComm/config.lua
+++ DiscordComm/config.lua
@@ -1,8 +1,8 @@
 Config = {}
 Config.ESX = false
-Config.BotToken = "NzAzMTI3Mjg5MzE1MTk2OTY5.XvVusw.SnYFXNArootdkopVv-4a2sZg9yY"
-Config.WebHook = "https://discordapp.com/api/webhooks/727065950033018901/OEh-Gez9iCnzG-2vBenl_7a1eNxw3GBzNAPSZfyUiBnrWvCi-U1fBVR7G8ZHuyhLZZTt"
-Config.ChannelID = "727065924565205022"
-Config.ReplyUserName = "In Game Bot"
-Config.AvatarURL = "https://cdn.discordapp.com/icons/701972474333495317/a_59c1ed6cd9f5dda7511d8862063e933f.webp?size=1024"
+Config.BotToken = "Your Bot Token"
+Config.WebHook = "Your Discord Webhook"
+Config.ChannelID = "Channel ID of the Commands"
+Config.ReplyUserName = "Bot Username"
+Config.AvatarURL = "Bot Avatar URL"
 Config.Prefix = "!"
